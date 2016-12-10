816d04b6b92f21dc43e23034f3859261b03b428f
Aleksandar Himel <aleksandarhimel@gmail.com>
2016-12-10 15:50:34 +0100

    Highlighting for gameobjects

---
+++ Assets/Scripts/HoverHighlighter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverHighlighter : MonoBehaviour {
+    Shader old;
+
+    void OnMouseEnter() {
+        old = GetComponent<Renderer>().material.shader;
+        GetComponent<Renderer>().material.shader = Shader.Find("Tessellation/Bumped Specular (smooth)");
+    }
+
+    void OnMouseExit() {
+        GetComponent<Renderer>().material.shader = old;
+    }
+
+}
