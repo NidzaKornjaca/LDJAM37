9085db2d3fe27343f7ab4b2adfd74b549416251a
Aleksandar Himel <aleksandarhimel@gmail.com>
2016-12-11 04:11:22 +0100

    Basic mechanics for the actual game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : Singleton<GameManager> {
+
+    public TriggerArea ta;
+    public GameObject[] namestaj;
+    public List<GameObject> trenutni;
+
+	// Use this for initialization
+	void Start () {
+
+        NextTarget();
+        
+        ta.Subscribe(Calculate);
+	}
+
+    void NextTarget() {
+        trenutni.Clear();
+        GameObject pom = namestaj[Random.Range(0, namestaj.Length)];
+        if (pom.transform.childCount > 0)
+        {
+            for (int i = 0; i < pom.transform.childCount; i++)
+                trenutni.Add(pom.transform.GetChild(i).gameObject);
+        }
+        else {
+            trenutni.Add(pom);
+        }
+        Debug.Log(pom.name);
+    }
+
+    void Calculate(Collider other) {
+        for (int i = 0; i < trenutni.Count; i++)
+        {
+            if (other.gameObject == trenutni[i])
+            {
+                trenutni.RemoveAt(i);
+                if(trenutni.Count == 0)
+                    NextTarget();
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/TriggerArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerArea : MonoBehaviour {
+
+    public class ColliderEvent : UnityEvent<Collider> { }
+
+    private ColliderEvent m_event;
+
+    void Awake() {
+        if (m_event == null) m_event = new ColliderEvent();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        m_event.Invoke(other);
+    }
+
+    public void Subscribe(UnityAction<Collider> action) {
+        m_event.AddListener(action);
+    }
+
+    public void UnSubsribe(UnityAction<Collider> action) {
+        m_event.RemoveListener(action);
+    }
+
+}
