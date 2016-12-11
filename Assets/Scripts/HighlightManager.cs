fb546ca5611190cf6ae87946906364e429f9f780
Aleksandar Himel <aleksandarhimel@gmail.com>
2016-12-11 01:55:27 +0100

    hl manager upgrade

---
+++ Assets/Scripts/HighlightManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightManager : MonoBehaviour {
+
+    private GameObject last = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera mainCamera = Camera.main;
+        RaycastHit hit = new RaycastHit();
+        if (
+            !Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition).origin,
+                             mainCamera.ScreenPointToRay(Input.mousePosition).direction, out hit, 2f,
+                             Physics.DefaultRaycastLayers))
+        {
+            if (last != null)
+            {
+                last.GetComponent<HoverHighlighter>().HighlightOff();
+                last = null;
+            }
+            return;
+        }
+        if (last == hit.collider.gameObject) { return; }
+        if (hit.collider.GetComponent<HoverHighlighter>())
+        {
+            if (last != null)
+            {
+                last.GetComponent<HoverHighlighter>().HighlightOff();
+                last = null;
+            }
+            last = hit.collider.gameObject;
+            last.GetComponent<HoverHighlighter>().HighlightOn();
+        }
+    }
+}
--- Assets/Scripts/HoverHighlighter.cs
+++ Assets/Scripts/HoverHighlighter.cs
@@ -5,28 +5,16 @@ using UnityEngine;
 public class HoverHighlighter : MonoBehaviour {
     Shader old;
 
-    void OnMouseEnter() {
-        Camera mainCamera = Camera.main;
+    public void HighlightOn() {
         old = GetComponent<Renderer>().material.shader;
-        RaycastHit hit = new RaycastHit();
-        if (
-            !Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition).origin,
-                             mainCamera.ScreenPointToRay(Input.mousePosition).direction, out hit, 100,
-                             Physics.DefaultRaycastLayers))
-        {
-            return;
-        }
-        if (hit.distance < 2f)
-        {
-            Renderer ren = GetComponent<Renderer>();
-            if (ren)
-                ren.material.shader = Shader.Find("Reflective/Parallax Diffuse");
-        }
+        Renderer ren = GetComponent<Renderer>();
+        if (ren)
+            ren.material.shader = Shader.Find("Reflective/Parallax Diffuse");
     }
 
-    void OnMouseExit() {
+    public void HighlightOff() {
         Renderer ren = GetComponent<Renderer>();
-        if(ren)
+        if (ren)
             ren.material.shader = old;
     }
 
