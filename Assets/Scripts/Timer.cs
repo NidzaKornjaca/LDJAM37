ff7e5de573df41d7d8216a45cf425509fd322181
Aleksandar Himel <aleksandarhimel@gmail.com>
2016-12-12 02:31:26 +0100

    Timer class added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour {
+
+    float timeLeft;
+    private UnityEvent timerOff;
+
+    void StartTimer(float time) {
+        timeLeft = time;
+        gameObject.SetActive(true);
+    }
+
+    public void Subscribe(UnityAction action) {
+        timerOff.AddListener(action);
+    }
+
+    public void Unsubscribe(UnityAction action) {
+        timerOff.RemoveListener(action);
+    }
+
+    void Start() {
+        gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0) {
+            timerOff.Invoke();
+            gameObject.SetActive(false);
+        }	
+	}
+}
